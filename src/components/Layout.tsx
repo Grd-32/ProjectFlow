0ffe2e55b8a498bb0eae6c42f7003e5c995c3ec1
Grd-32 <82405588+Grd-32@users.noreply.github.com>
2025-09-12 12:43:56 +0300

    Updated Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import TopNavigation from './TopNavigation';
+import ResponsiveLayout from './ResponsiveLayout';
+import MobileNavigation from './MobileNavigation';
+import VoiceCommands from './VoiceCommands';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <ResponsiveLayout
+        sidebar={<Sidebar />}
+        topNav={<TopNavigation />}
+      >
+        <div className="pb-16 md:pb-0">
+          {children}
+        </div>
+      </ResponsiveLayout>
+      <MobileNavigation />
+      <VoiceCommands />
+    </>
+  );
+};
+
+export default Layout;
